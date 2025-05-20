df8430f9ff49bd5232d26500a5403264aa37bdae
akhila2711 <akhilakhi112799@gmail.com>
2025-05-20 05:55:13 +0530

    add

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const app = express();
 const bodyParser = require('body-parser'); //import body-parser
 app.use(bodyParser.urlencoded()); //use body-parser to parse urlencoded data
 
-app.use('/add-product',(req,res,next)=>{   //use is a middleware function--3 parameters--req,res,next
+app.get('/add-product',(req,res,next)=>{   //use is a middleware function--3 parameters--req,res,next
    res.send('<form action="/show-product" method="POST"><input type="text" name="title"><button type="submit" value="submit">Add Product</button></form>'); //send response to the client
     
 });
-app.use('/show-product',(req,res,next)=>{
+app.post('/show-product',(req,res,next)=>{
     console.log("form data",req.body);
+    res.send('Product Added Successfully');
   
 });
 
