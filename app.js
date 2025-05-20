fb8ac186e69c784c7bc7c5596384964cd52b02f9
akhila2711 <akhilakhi112799@gmail.com>
2025-05-20 06:25:51 +0530

    add

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 const express = require('express'); //import express
 const app = express();
 const bodyParser = require('body-parser'); //import body-parser
+const adminRoutes = require('./routes/admin'); //import admin routes
+app.use(adminRoutes); //use admin routes
 app.use(bodyParser.urlencoded()); //use body-parser to parse urlencoded data
 app.listen(8000)
+
 
 
 //Middle ware
--- routes/admin.js
+++ routes/admin.js
@@ -2,13 +2,17 @@
 const express = require('express'); //import express
 
 const router = express.Router(); //create a router object
-app.get('/add-product',(req,res)=>{   //use is a middleware function--3 parameters--req,res,next
+
+const bodyParser = require('body-parser'); //import body-parser
+
+router.get('/add-product',(req,res)=>{   //use is a middleware function--3 parameters--req,res,next
    res.send('<form action="/show-product" method="POST"><input type="text" name="title"><button type="submit" value="submit">Add Product</button></form>'); //send response to the client
     
 });
-app.post('/show-product',(req,res)=>{
+router.post('/show-product',(req,res)=>{
     console.log("form data",req.body);
     res.send('<h1>Product Added Successfully</h1>');
   
 });
+
 module.exports = router; //export the router object
